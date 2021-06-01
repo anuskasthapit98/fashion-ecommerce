b324781bc24d3bacee2a3c6caa3d414f84860d85
Anuska Sthapit <anuskasthapit@sxc.edu.np>
2021-06-01 15:55:00 +0545

    models

--- dashboard/models.py
+++ dashboard/models.py
@@ -22,55 +22,58 @@ class TimeStamp(models.Model):
             return super().delete()
         
 class Category(TimeStamp):
-    category_name = models.CharField(max_length=250)
-    category_img = models.ImageField(upload_to="category")
+    name = models.CharField(max_length=250)
+    img = models.ImageField(upload_to="category")
     
     class Meta:
         verbose_name = ('Category')
         verbose_name_plural = ('Categories')
         
     def __str__(self):
-        return self.category_name
+        return self.name
     
 class SubCategory(TimeStamp):
     category = models.ForeignKey(Category, on_delete=models.CASCADE )
-    sub_category_name= models.CharField(max_length=250)
-    sub_category_img = models.ImageField(upload_to="subcategory")
+    name= models.CharField(max_length=250)
+    img = models.ImageField(upload_to="subcategory")
         
     class Meta:
         verbose_name = ('SubCategory')
         verbose_name_plural = ('SubCategories')
     
     def __str__(self):
-        return self.sub_category_name   
+        return self.name   
     
 class Brands(TimeStamp):
-    brand_name = models.CharField(max_length=250)
-    brand_logo =  models.ImageField(upload_to="brands") 
+    name = models.CharField(max_length=250)
+    logo =  models.ImageField(upload_to="brands") 
     
     class Meta:
         verbose_name = ('Brand')
         verbose_name_plural = ('Brands')
         
     def __str__(self):
-        return self.brand_name
+        return self.name
 
         
 class Products(TimeStamp):
     categories = models.ForeignKey(Category, on_delete=models.CASCADE)
     brands = models.ForeignKey(Brands, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=250)
-    product_image = models.ImageField(upload_to="products") 
+    name = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="products") 
     description = RichTextField()
     price = models.PositiveIntegerField()
     status= models.BooleanField (default= True)
+    slug = models.SlugField(unique= True, primary_key= True)
+    discount = models.PositiveIntegerField()
+    view_count = models.PositiveIntegerField(default= 0)
     
     class Meta:
         verbose_name = ('Product')
         verbose_name_plural = ('Products')
         
     def __str__(self):
-        return self.product_name
+        return self.name
     
 class Account(User):
     mobile = models.CharField(max_length=250)
