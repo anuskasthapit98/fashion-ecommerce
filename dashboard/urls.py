e2d36823c5ae799037c52da9793d2671ef7505d4
Anuska Sthapit <anuskasthapit@sxc.edu.np>
2021-06-02 14:06:53 +0545

    category crud

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,12 @@ from .views import *
 
 app_name = 'dashboard'
 urlpatterns = [
-    path('dashboard/', AdminDashboardView.as_view(), name="admin- dashboard"),
-
+    path('dashboard/', AdminDashboardView.as_view(), name="admin-dashboard"),
+    
+    
+    #Category
+    path('category/', CategoryListView.as_view(), name = "category"),
+    path('category/create/', CategoryListView.as_view(), name = "category_create"),
+    path('category/update/<int:pk>/', CategoryListView.as_view(), name = "category_update"),
+    path('category/delete/<int:pk>/', CategoryListView.as_view(), name = "category_delete"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,17 @@
+from re import template
+from dashboard.models import Category
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import *
 # Create your views here.
 
 class AdminDashboardView(TemplateView):
 	template_name = 'dashboard/base/index.html'
+
+
+
+class CategoryListView(ListView):
+    Model = Category
+    template_name = 'dashboard/Category/list.html'
+    context_object_name = 'category'
+    
